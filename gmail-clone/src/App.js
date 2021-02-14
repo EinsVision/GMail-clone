102ae9fe2fff19a4179d0fc893eaebb721bb0284
jungchangdae <einsvision@gmail.com>
2021-02-15 02:06:43 +0900

    First Gmail-clone

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Gmail-clone</h1>
+      <Header />
+      <h1>Gmail-clone!</h1>
     </div>
   );
 }
