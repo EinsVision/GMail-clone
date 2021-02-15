b271e414c1a59ce685420aa75b0dbf4bd5fd7446
jungchangdae <einsvision@gmail.com>
2021-02-15 13:28:42 +0900

    react-router-dom 추가

--- gmail-clone/src/Sidebar.js
+++ gmail-clone/src/Sidebar.js
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
 import AddIcon from '@material-ui/icons/Add';
@@ -10,6 +10,9 @@ import NearMeIcon from '@material-ui/icons/NearMe';
 import LabelImportantIcon from '@material-ui/icons/LabelImportant';
 import NoteIcon from '@material-ui/icons/Note';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PersonIcon from '@material-ui/icons/Person';
+import DuoIcon from '@material-ui/icons/Duo';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 function Sidebar() {
   return (
@@ -22,7 +25,24 @@ function Sidebar() {
       <SidebarOption Icon={NearMeIcon} title='Sent' number={54} />
       <SidebarOption Icon={NoteIcon} title='Drafts' number={54} />
       <SidebarOption Icon={ExpandMoreIcon} title='More' number={54} />
+
+      <div className='sidebar__footer'>
+        <div className='sidebar__footerIcon'>
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
+    
   )
 }
 
