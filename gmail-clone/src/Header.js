1fb4890e2a02c6087ec712a4686394445d0d7dff
jungchangdae <einsvision@gmail.com>
2021-02-15 10:47:28 +0900

    Header design

--- gmail-clone/src/Header.js
+++ gmail-clone/src/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
 
 function Header() {
     return (
         <div className='header'>
-            <h1>This is header.</h1>
+            <div className='header__left'>
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src='https://logodownload.org/wp-content/uploads/2018/03/gmail-logo-1-1.png' alt='' />
+            </div>
+
+            <div className='header__middle'>
+                <SearchIcon />
+                <input placeholder='Search mail' type='text'/>
+                <ArrowDropDownIcon className='header__inputCaret' />
+            </div>
+
+            <div className='header__right'>
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+            </div>
+            
+            
         </div>
     )
 }
