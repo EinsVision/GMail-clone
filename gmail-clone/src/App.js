a5e1802354d2cb0be779b3559988a91f369ee3a3
EinsVision <einsvision@gmail.com>
2021-02-16 13:33:51 +0900

    login logout using firebase

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -12,14 +12,29 @@ import {
 import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
-import { selectUser } from './features/userSlice';
+import { login, selectUser } from './features/userSlice';
 import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged( user => {
+      if(user){
+        // the user is logged in
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        }))
+      }
+    });
+  }, [])
 
   return (
     <Router>
